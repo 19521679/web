42c834b3578f4d39e3ff65c2ac6756ba53203efb
Khang Nguyễn Lê Nguyên <19521666@ms.uit.edu.vn>
2021-11-18 20:21:05 +0700

    create component Accounts

---
+++ frond-end/Accounts/routes.js
@@ -0,0 +1,34 @@
+import ThongTinTaiKhoan from "./ThongTinTaiKhoan";
+import ThongBaoCuaToi from "./ThongBaoCuaToi";
+import QuanLyDonHang from "./QuanLyDonHang";
+import SoDiaChi from "./SoDiaChi";
+import SanPhamYeuThich from "./SanPhamYeuThich";
+
+const routes=[
+    {
+        path: "/lmember/thongtintaikhoan",
+        exact: true,
+        main: () => <ThongTinTaiKhoan></ThongTinTaiKhoan>
+      },
+      {
+        path: "/lmember/quanlydonhang",
+        exact: true,
+        main: () => <QuanLyDonHang></QuanLyDonHang>
+      },
+      {
+        path: "/lmember/thongbaocuatoi",
+        exact: true,
+        main: () => <ThongBaoCuaToi></ThongBaoCuaToi>
+      },
+      {
+        path: "/lmember/sodiachi",
+        exact: true,
+        main: () => <SoDiaChi></SoDiaChi>
+      },
+      {
+        path: "/lmember/sanphamyeuthich",
+        exact: true,
+        main: () => <SanPhamYeuThich></SanPhamYeuThich>
+      },
+]
+export default routes;
