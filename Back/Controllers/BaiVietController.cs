2a2ff579b181da0fc234c33c14cece9f47f4bb22
unknown <vqh.official@gmail.com>
2021-11-26 20:47:26 +0700

    get article list from db

---
+++ Back/Controllers/BaiVietController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Back.Models.Account;
+using Back.Models;
+using Newtonsoft.Json;
+using Microsoft.EntityFrameworkCore;
+
+namespace Back.Controllers
+{
+    public class BaiVietController : Controller
+    {
+        private readonly ILogger<BaiVietController> _logger;
+        lavenderContext lavenderContext;
+        public BaiVietController(ILogger<BaiVietController> logger, lavenderContext lavenderContext)
+        {
+            _logger = logger;
+            this.lavenderContext = lavenderContext;
+        }
+
+        [Route("/baiviet")]
+        [HttpGet]
+        public async Task<IActionResult> GetAllBaiViet()
+        {
+            var baivietlist = await (from bv in lavenderContext.Baiviets
+                                              select bv).ToListAsync();
+         /*   foreach (var i in baivietlist)
+            {
+                var e = lavenderContext.Entry(i);
+                await e.Reference(x => x.ManhacungcapNavigation).LoadAsync();
+                await e.Reference(x => x.MasanphamNavigation).LoadAsync();
+            }*/
+           return StatusCode(200, Json(baivietlist));
+          //   return StatusCode(200, baivietlist);
+
+        }
+    }
+}
